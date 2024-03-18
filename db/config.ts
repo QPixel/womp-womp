05f87ce0b200121e5d7cad29c7a228100258dba0
Riley Smith <riley@rileysmith.me>
2024-03-18 15:52:26 -0700

    fix schema issues

--- db/config.ts
+++ db/config.ts
@@ -3,6 +3,7 @@ import { column, defineDb, defineTable } from 'astro:db';
 const Womps = defineTable({
   columns: {
     lastUpdated: column.date({default: new Date(), deprecated: true, optional: true}),
+    total: column.number({default: 0, deprecated: true, optional: true}),
     last_updated: column.date({default: new Date()}),
     id: column.number({primaryKey: true}),
     updated_by: column.number({default: 0}),
