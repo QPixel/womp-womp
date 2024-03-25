73e5cba45ba25d7882917907f729e48e40a0b52f
Riley Smith <riley@rileysmith.me>
2024-03-24 17:01:34 -0700

    better way of sharing a link

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,24 @@ import { defineMiddleware } from "astro:middleware";
 export const onRequest = defineMiddleware(({request, cookies, redirect}, next) => {
     const url = new URL(request.url);
     const query = url.searchParams.get('id'); 
-    if (query && url.pathname === "/") {
+    let idRegex = url.pathname.match(/\/([\d]+)/g);
+    if (idRegex) {
+        cookies.set('id', idRegex[0].replace('/', ''), {
+            path: '/',
+            expires: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 365),
+        });
+        cookies.set('triedToIncrement', '0', {
+            path: '/',
+        
+        });
+        cookies.set('resetAt', new Date(new Date().getTime() + 1000 * 60 * 60 * 24).toISOString(), {
+            path: '/',
+
+        });
+        return next();
+    }
+
+    if ((query && url.pathname === "/" )) {
         cookies.set('id', query, {
             path: '/',
             expires: new Date(new Date().getTime() + 1000 * 60 * 60 * 24 * 365),
@@ -16,13 +33,7 @@ export const onRequest = defineMiddleware(({request, cookies, redirect}, next) =
             path: '/',
 
         });
-        if (query === '2') {
-            console.log("maddie requested", query, url, request);
-        }
         return redirect('/');
-    }
-    if (query === '2') {
-        console.log("maddie requested", query, url, request);
     }
     return next();
 });
