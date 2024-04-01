e5339560aeecbdad2f7da76266e910cf78dc4252
Riley Smith <riley@rileysmith.me>
2024-04-01 10:35:26 -0700

    added new data structure lol

--- src/lib/components/get-leaderboard.ts
+++ src/lib/components/get-leaderboard.ts
@@ -1,6 +1,7 @@
 import { writable } from "svelte/store";
 import type { LeaderboardData } from "src/pages/api/leaderboard/all";
 
+
 function getLeaderboard() {
     const { subscribe, set, update } = writable<LeaderboardData>();
 
@@ -12,18 +13,19 @@ function getLeaderboard() {
         update_local: (id: number, resolved_username = "", current_quarter = "") => {
             update((v) => {
                 
-                if (!v[current_quarter]) {
-                    v[current_quarter] = [];
+                if (!v[current_quarter] || !v[current_quarter].data) {
+                    v[current_quarter].data = [];
                 }
 
-                let newData = v[current_quarter];
+                let newData = v[current_quarter].data;
                 if (!newData.some((entry) => entry.updated_by === id)) {
                     newData.push({
                         updated_by: id,
                         total: 1,
                         resolved_username: resolved_username !== "" ? resolved_username : "Unknown",
                     });
-                    v[current_quarter] = newData.sort((a, b) => b.total - a.total);
+                    v[current_quarter].data = newData.sort((a, b) => b.total - a.total);
+                    v[current_quarter].total = v[current_quarter].total + 1;
                 }
 
                 if (newData.some((entry) => entry.updated_by === id)) {
@@ -36,7 +38,8 @@ function getLeaderboard() {
                         }
                         return entry;
                     });
-                    v[current_quarter] = newData.sort((a, b) => b.total - a.total);
+                    v[current_quarter].data = newData.sort((a, b) => b.total - a.total);
+                    v[current_quarter].total = v[current_quarter].total + 1;
                 }
                 return v;
             })
--- src/pages/api/leaderboard/all.ts
+++ src/pages/api/leaderboard/all.ts
@@ -11,24 +11,36 @@ const kv = createClient({
     token: REDIS_REST_API_TOKEN,
 });
 
+
+
 export async function getLeaderboard() {
     let wompTotals = await db.select({
         updated_by: Womps.updated_by,
         total: sql<number>`count(*)`.as("total"),
         quarter: Womps.quarter_id,
     }).from(Womps).groupBy(Womps.quarter_id, Womps.updated_by).orderBy(desc(sql`total`)).limit(10);
-
-    let wompData: Record<string, Array<Omit<typeof wompTotals[0], "quarter"> & {resolved_username: string}>> = {}
+    let womp = await db.select({
+        total: sql<number>`count(*)`.as("total"),
+        quarter: Womps.quarter_id,
+    }).from(Womps).groupBy(Womps.quarter_id).orderBy(desc(sql`total`));
+    console.log(womp);
+    let wompData: Record<string, {
+        data: Array<Omit<typeof wompTotals[0], "quarter"> & {resolved_username: string}>;
+        total: number;
+    }> = {}
     for (let womp of wompTotals) {
         let username = await kv.get<string>(`user:${womp.updated_by}`);
         if (!wompData[womp.quarter]) {
-            wompData[womp.quarter] = [];
+            wompData[womp.quarter] = { data: [], total: 0 };
         }
-        wompData[womp.quarter].push({
+        wompData[womp.quarter].data.push({
             total: womp.total,
             updated_by: womp.updated_by,
             resolved_username: username ? username : "Unknown",
         });
+    }
+    for (let total of womp) {
+        wompData[total.quarter].total = total.total;
     }
 
     return wompData;
