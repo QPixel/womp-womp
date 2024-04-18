bdf7aba2985500dc548ffd9ea7e4f763601ba226
Riley Smith <riley@rileysmith.me>
2024-04-18 13:30:08 -0700

    auto dark and light mode

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	darkMode: ["class"],
+	darkMode: "media",
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	safelist: ["dark"],
 	theme: {
